6404f065744c730cde9b37a8a0ecafed22dce0d4
OceanLion <doubter-carbons0g@icloud.com>
2025-08-25 21:27:25 -0500

    exercise 2 all tests passed

--- exercises/e2 (# Edit conflict 2025-08-25 kKpvoRs #).js
+++ exercises/e2 (# Edit conflict 2025-08-25 kKpvoRs #).js
@@ -44,7 +44,8 @@ function totalRoomsArea(area1, area2, area3) {
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
 const result = function () {
-    console.log(totalRoomsArea(room1, room2, room3));
+  return totalRoomsArea(room1, room2, room3);
+    console.log(result());
 }
 
 
