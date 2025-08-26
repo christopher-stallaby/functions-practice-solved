b8acab122191d81a3bc44252ea440340189c039f
OceanLion <doubter-carbons0g@icloud.com>
2025-08-25 22:06:18 -0500

    exercises 4 and 5 all tests passed

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,10 +9,7 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
-  return str.split(' ');
-}
-
+const splitFirstAndLastNames = (str) => str.split(' ');
 
 /** =========================
  * The personAge() constant is assigned to a function, that takes an object of person Data as the argument.
@@ -20,9 +17,7 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
-  return personObject.age;
-}
+const personAge = (personObject) => personObject.age;
 
 
 /** =========================
@@ -31,10 +26,7 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'Michael') => true;
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
-
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
-}
+const isNameInArray = (arr, name) => arr.includes(name);
 
 
 /** =========================
@@ -44,9 +36,9 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+const logSecondsUpToMax = (max) => {
   let i = 0;
-  const timer = setInterval(function() {
+  const timer = setInterval(() => {
     if (i < max) {
       console.log(++i);
     } else {
@@ -54,7 +46,6 @@ const logSecondsUpToMax = function(max) {
     }
   }, 1000);
 }
-
 
 
 // === TEST YOURSELF ===
